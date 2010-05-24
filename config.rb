cd2e9345e7d85f4f3186c5e5d9f197a5c3142300
Mark Young <zarzax@gmail.com>
2010-05-24 13:19:33 -0500

    Initial commit of famfam mini icon sprite.  Sass mixin.

---
+++ config.rb
@@ -0,0 +1,12 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "src"
+images_dir = ""
+output_style = :expanded
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+line_comments = false
+environment = :production
